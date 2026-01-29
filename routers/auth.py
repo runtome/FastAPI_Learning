a595b0ef6238dbeb5c21022da1d19ebbf657a7e8
SUPHOT NARAPONG <suphot.n@gmail.com>
2026-01-29 10:33:26 +0700

    Making Create User

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,28 @@
 from fastapi import APIRouter
 from starlette import status
+from pydantic import BaseModel
+from models import Users
 
 
 router = APIRouter()
 
-@router.get("/auth/", status_code=status.HTTP_200_OK)
-async def get_user():
-    return {"user": "authenticated_user"}
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+@router.post("/auth/", status_code=status.HTTP_200_OK)
+async def create_user(create_user_request: CreateUserRequest):
+    user = Users(
+        username=create_user_request.username,
+        email=create_user_request.email,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True
+    )
+    return user
