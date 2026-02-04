7aac41e7187feeb339d1a85ab67045d009597a51
SUPHOT NARAPONG <suphot.n@gmail.com>
2026-02-04 10:20:49 +0700

    Update and delete

--- test/test_todos.py
+++ test/test_todos.py
@@ -110,4 +110,50 @@ def test_create_todo():
     assert todo_in_db.complete == request_data["complete"]
     db.close()
 
+
+def test_update_todo(test_todo):
+    todo_id = test_todo.id
+    update_data = {
+        "title": "Updated Todo",
+        "description": "This todo has been updated",
+        "priority": 3,
+        "complete": True
+    }
+    response = client.put(f"/todo/{todo_id}/", json=update_data)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    db = TestingSessionLocal()
+    updated_todo = db.query(Todos).filter(Todos.id == todo_id).first()
+    assert updated_todo.title == update_data["title"]
+    assert updated_todo.description == update_data["description"]
+    assert updated_todo.priority == update_data["priority"]
+    assert updated_todo.complete == update_data["complete"]
+    db.close()
  
+ 
+def test_update_todo_not_found(test_todo):
+    todo_id = 999  # Non-existent todo ID
+    update_data = {
+        "title": "Updated Todo",
+        "description": "This todo has been updated",
+        "priority": 3,
+        "complete": True
+    }
+    response = client.put(f"/todo/{todo_id}/", json=update_data)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+def test_delete_todo(test_todo):
+    todo_id = test_todo.id
+    response = client.delete(f"/todo/{todo_id}")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    db = TestingSessionLocal()
+    deleted_todo = db.query(Todos).filter(Todos.id == todo_id).first()
+    assert deleted_todo is None
+    db.close()
+    
+def test_delete_todo_not_found():
+    todo_id = 999  # Non-existent todo ID
+    response = client.delete(f"/todo/{todo_id}")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()["detail"] == "Todo not found"
